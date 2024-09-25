f2a0de4995aa35f5e90531a0328946ab82ff53e2
steinm58 <steinm58@msu.edu>
2024-09-25 11:52:27 -0400

    initialized react and started header content

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="lexend-mega name">mackenzi steinmetz</h1>
+        <nav>
+          <ul>
+            <li>home</li>
+            <li>resume</li>
+            <li>projects</li>
+            <li>about</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
