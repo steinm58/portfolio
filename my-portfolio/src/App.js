7f4335ead31ee3bcdc36a08bba22abe193b6081f
steinm58 <steinm58@msu.edu>
2024-09-25 14:59:21 -0400

    navagation styling

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,14 +5,17 @@ function App() {
     <div className="App">
       <header>
         <h1 className="lexend-mega name">mackenzi steinmetz</h1>
-        <nav>
-          <ul>
-            <li>home</li>
-            <li>resume</li>
-            <li>projects</li>
-            <li>about</li>
-          </ul>
-        </nav>
+        <div className="nav">
+          <nav className="lexend">
+            <ul>
+              <li>home</li>
+              <li>resume</li>
+              <li>projects</li>
+              <li>about</li>
+            </ul>
+          </nav>
+        </div>
+        <hr></hr>
       </header>
     </div>
   );
